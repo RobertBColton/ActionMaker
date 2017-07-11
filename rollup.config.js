c41489bf9c42644c6516fd391bf979616c51e9c3
RobertBColton <robertbcolton@gmail.com>
2017-07-11 04:10:18 -0400

    fix this whitespace nonsense, use tabs size 4

--- rollup.config.js
+++ rollup.config.js
@@ -3,19 +3,19 @@ import resolve from 'rollup-plugin-node-resolve';
 import css from 'rollup-plugin-css-only';
 
 export default {
-  entry: './lib/index.js',
-  dest: './bin/index.js',
-  format: 'iife',
-  moduleName: 'ActionMaker',
-  sourceMap: false,
-  useStrict: false,
-  intro: "process = { env: { NODE_ENV: window } };",
-  globals: {
-    vue: 'Vue'
-  },
-  plugins: [
-    vue({styleToImports: true}),
-    css({output: './bin/index.css'}),
-    resolve()
-  ]
+	entry: './lib/index.js',
+	dest: './bin/index.js',
+	format: 'iife',
+	moduleName: 'ActionMaker',
+	sourceMap: false,
+	useStrict: false,
+	intro: "process = { env: { NODE_ENV: window } };",
+	globals: {
+		vue: 'Vue'
+	},
+	plugins: [
+		vue({styleToImports: true}),
+		css({output: './bin/index.css'}),
+		resolve()
+	]
 }
