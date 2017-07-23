eee8c19e6c186b7fc91e685b4cbfe6e2a90ae834
RobertBColton <robertbcolton@gmail.com>
2017-07-23 07:02:44 -0400

    Ensure build directory exists

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,14 @@ import vue from 'rollup-plugin-vue';
 import resolve from 'rollup-plugin-node-resolve';
 import css from 'rollup-plugin-css-only';
 import replace from 'rollup-plugin-replace';
+import mkdirp from 'mkdirp';
+
+// ensure output directory exists
+mkdirp('bin');
 
 export default {
-	entry: './lib/index.js',
-	dest: './bin/index.js',
+	entry: 'lib/index.js',
+	dest: 'bin/index.js',
 	format: 'iife',
 	moduleName: 'ActionMaker',
 	sourceMap: false,
@@ -15,7 +19,7 @@ export default {
 	},
 	plugins: [
 		vue({styleToImports: true}),
-		css({output: './bin/index.css'}),
+		css({output: 'bin/index.css'}),
 		resolve(),
 		replace({
 			'process.env.NODE_ENV': JSON.stringify( 'development' )
